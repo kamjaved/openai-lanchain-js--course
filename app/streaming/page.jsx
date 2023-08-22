f9d7d304459b193ce94058c6708f3d2589ec909c
KAMRAN <kamranjaved1234@gmail.com>
2023-08-22 21:58:26 +0530

    streaming-app

--- app/streaming/page.jsx
+++ app/streaming/page.jsx
@@ -1,66 +1,92 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import PageHeader from "../components/PageHeader";
-import PromptBox from "../components/PromptBox";
-import ResultStreaming from "../components/ResultStreaming";
-import Title from "../components/Title";
-import TwoColumnLayout from "app/components/TwoColumnLayout";
+'use client';
+import React, { useState, useEffect } from 'react';
+import PageHeader from '../components/PageHeader';
+import PromptBox from '../components/PromptBox';
+import ResultStreaming from '../components/ResultStreaming';
+import Title from '../components/Title';
+import TwoColumnLayout from 'app/components/TwoColumnLayout';
 
 const Streaming = () => {
-  const [prompt, setPrompt] = useState("");
-  const [error, setError] = useState(null);
-  const [data, setData] = useState("");
-  //   add code
+	const [prompt, setPrompt] = useState('');
+	const [error, setError] = useState(null);
+	const [data, setData] = useState('');
+	const [source, setSource] = useState(null);
+	//   add code
 
-  const processToken = (token) => {
-    // add code
-    return;
-  };
+	const processToken = (token) => {
+		return token.replace(/\\n/g, '\n').replace(/\"/g, '');
+	};
 
-  const handlePromptChange = (e) => {
-    setPrompt(e.target.value);
-  };
+	const handlePromptChange = (e) => {
+		setPrompt(e.target.value);
+	};
 
-  const handleSubmit = async () => {
-    try {
-      //   add code
-    } catch (err) {
-      console.error(err);
-      setError(error);
-    }
-  };
+	const handleSubmit = async () => {
+		try {
+			console.log(`Sending ${prompt}`);
 
-  // Clean up the EventSource on component unmount
-  //   add code
-  return (
-    <>
-      <Title emoji="💭" headingText="Streaming" />
-      <TwoColumnLayout
-        leftChildren={
-          <>
-            <PageHeader
-              heading="Spit a Rap."
-              boldText="Nobody likes waiting for APIs to load. Use streaming to improve the user experience of chat bots."
-              description="This tutorial uses streaming.  Head over to Module X to get started!"
-            />
-          </>
-        }
-        rightChildren={
-          <>
-            <ResultStreaming data={data} />
-            <PromptBox
-              prompt={prompt}
-              handlePromptChange={handlePromptChange}
-              handleSubmit={handleSubmit}
-              placeHolderText={"Enter your name and city"}
-              error={error}
-              pngFile="pdf"
-            />
-          </>
-        }
-      />
-    </>
-  );
+			await fetch('/api/streaming', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ input: prompt }),
+			});
+
+			// close existing sources
+			if (source) {
+				source.close();
+			}
+
+			// create new eventSource
+			const newSource = new EventSource('/api/streaming'); // learn more about EventSource in document.md
+			setSource(newSource);
+
+			newSource.addEventListener('newToken', (event) => {
+				const token = processToken(event.data);
+				setData((prevData) => prevData + token);
+			});
+
+			newSource.addEventListener('end', () => {
+				newSource.close();
+			});
+		} catch (err) {
+			console.error(err);
+			setError(error);
+		}
+	};
+
+	// Clean up the EventSource on component unmount
+	//   add code
+	return (
+		<>
+			<Title emoji="💭" headingText="Streaming" />
+			<TwoColumnLayout
+				leftChildren={
+					<>
+						<PageHeader
+							heading="Spit a Rap."
+							boldText="Nobody likes waiting for APIs to load. Use streaming to improve the user experience of chat bots."
+							description="This tutorial uses streaming.  Head over to Module X to get started!"
+						/>
+					</>
+				}
+				rightChildren={
+					<>
+						<ResultStreaming data={data} />
+						<PromptBox
+							prompt={prompt}
+							handlePromptChange={handlePromptChange}
+							handleSubmit={handleSubmit}
+							placeHolderText={'Enter your name and city'}
+							error={error}
+							pngFile="pdf"
+						/>
+					</>
+				}
+			/>
+		</>
+	);
 };
 
 export default Streaming;
--- pages/api/streaming.js
+++ pages/api/streaming.js
@@ -1,25 +1,41 @@
-import { OpenAI } from "langchain/llms/openai";
-import SSE from "express-sse";
+import { OpenAI } from 'langchain/llms/openai';
+import SSE from 'express-sse';
 
 const sse = new SSE();
 
 export default function handler(req, res) {
-  if (req.method === "POST") {
-    const { input } = req.body;
+	if (req.method === 'POST') {
+		const { input } = req.body;
 
-    if (!input) {
-      throw new Error("No input");
-    }
-    // Initialize model
+		if (!input) {
+			throw new Error('No input');
+		}
+		// Initialize model
+		const chatModal = new OpenAI({
+			// NOTE: Learn About What other parameter of OpenAI like temprature,streaming,callbacks bard or in chatGPT research
+			streaming: true,
+			callbacks: [
+				{
+					handleLLMNewToken(token) {
+						sse.send(token, 'newToken');
+					},
+				},
+			],
+		});
 
-    // create the prompt
+		// create the prompt
+		const prompt = `Create a short rap about my name nad city. Make it funny. add hindi on it. Name: ${input}`;
 
-    // call frontend to backend
+		console.log({ prompt });
+		// call frontend to backend
+		chatModal.call(prompt).then(() => {
+			sse.send(null, 'end'); // this line ensure that once we created our rap song send nothing and end eventsource.
+		});
 
-    return res.status(200).json({ result: "OK" });
-  } else if (req.method === "GET") {
-    sse.init(req, res);
-  } else {
-    res.status(405).json({ message: "Method not allowed" });
-  }
+		return res.status(200).json({ result: 'Streaming Complete.' });
+	} else if (req.method === 'GET') {
+		sse.init(req, res);
+	} else {
+		res.status(405).json({ message: 'Method not allowed' });
+	}
 }
